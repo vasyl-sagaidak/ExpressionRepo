911b60a24fe2ab6a13d425b5febb146c39dfdbe7
Vasyl Sagaidak <saikopixie@gmail.com>
2024-12-29 13:53:31 +0200

    Add app version

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -3,5 +3,6 @@ package app;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Java ExpressionRepo");
+        System.out.println("Version 1.0.0");
     }
 }
