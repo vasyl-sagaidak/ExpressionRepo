d9f13fcfc11fca343fff5be646ba7b39bab91f69
Vasyl Sagaidak <saikopixie@gmail.com>
2024-12-29 13:55:40 +0200

    Add summing functionality

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -4,5 +4,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Java ExpressionRepo");
         System.out.println("Version 1.0.0");
+
+        int a = 10;
+        int b = 100;
+        System.out.println(sum(a, b));
+    }
+
+    public static int sum(int a, int b) {
+        return a + b;
     }
 }
