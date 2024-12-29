babcedb36bd00a47161acc8ada6afb70fb48e958
Vasyl Sagaidak <saikopixie@gmail.com>
2024-12-29 14:04:24 +0200

    Add description method

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -4,13 +4,19 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Java ExpressionRepo");
         System.out.println("Version 1.0.0");
+        description();
 
-        int a = 10;
-        int b = 100;
+        int a = 56;
+        int b = 8;
         System.out.println(sum(a, b));
     }
 
     public static int sum(int a, int b) {
         return a + b;
+    }
+
+    public static void description() {
+        System.out.println("This code is to implement and improve " +
+                "skills of using GitHub instruments and concepts.");
     }
 }
