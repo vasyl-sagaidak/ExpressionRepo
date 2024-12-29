51d1ccc718b661f014ab12590994d1a8f4070c99
Vasyl Sagaidak <saikopixie@gmail.com>
2024-12-29 13:58:23 +0200

    Add subdivision

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -2,12 +2,13 @@ package app;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to Java ExpressionRepo");
-        System.out.println("Version 1.0.0");
+        System.out.println("Welcome to Java Alternate Expressions");
+        System.out.println("Version 1.0.1");
 
         int a = 10;
         int b = 100;
-        System.out.println(sum(a, b));
+        int c = 5;
+        System.out.println(sum(a, b) / c);
     }
 
     public static int sum(int a, int b) {
